032654ebe5e0b195ce7b73d4646d9159b0c019e0
Jacob Floisand <jacobfloisand@gmail.com>
2019-06-17 19:11:36 -0600

    Fill in service interface and begin implementation of GetFullFile

--- FilerService2.0/FilerService2.0.svc.cs
+++ FilerService2.0/FilerService2.0.svc.cs
@@ -11,8 +11,27 @@ namespace FilerService2._0
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class FilerService2 : IFilerService2
     {
-        public void DoWork()
+        public void AddFile(ResourceDataVerbose data)
         {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(DeleteData Nickname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResourceDataVerbose[] DoSearch(string Class, string Unit, string Type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileContents GetFullFile(string Name, string Class, string Cookie)
+        {
+            //Select Archive from Cookies Natural Join UserIDs Natural Join Classes Natural Join Files where Cookie equals @Cookie && Class equals @Class && Name equals @Name
+            //Line above is not correct. See below for a start.
+            //SELECT Files.DataID FROM Files WHERE DataID IN (SELECT Classes.DataID FROM Classes WHERE Classes.Class = 'Biology') AND Name = '1210 articles_s18.pdf';
+            throw new NotImplementedException();
         }
     }
 }
--- FilerService2.0/IFilerService2.0.cs
+++ FilerService2.0/IFilerService2.0.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace FilerService2._0
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IFilerService2
     {
-        [OperationContract]
-        void DoWork();
+
+        [WebInvoke(Method = "POST", UriTemplate = "/save")]
+        void AddFile(ResourceDataVerbose data);
+
+        [WebInvoke(Method = "POST", UriTemplate = "/delete")]
+        void Delete(DeleteData Nickname);
+
+        [WebGet(UriTemplate = "/File?Name={Name}&Class={Class}&Cookie={Cookie}")]
+        FileContents GetFullFile(String Name, String Class, String Cookie);
+
+        [WebGet(UriTemplate = "/Search?Class={Class}&Unit={Unit}&Type={Type}")]
+        ResourceDataVerbose[] DoSearch(String Class, String Unit, String Type);
     }
 }
