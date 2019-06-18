ea4551d92ae12e3cc0cf03dcb8273a9e7a13c98a
Jacob Floisand <jacobfloisand@gmail.com>
2019-06-17 18:20:53 -0600

    Create Repo. Create db and fill in tables(copied from original server). Create DataModels.cs and fill in dataModels that will be needed in this solution.

---
+++ FilerService2.0/DataModels.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace FilerService2._0
+{
+
+    /// <summary>
+    /// This Data model is used when sending search result data to the client OR when the client is uploading a new resource to the server.
+    /// </summary>
+    [DataContract]
+    public class ResourceDataVerbose
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public string File { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Link { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string FileName { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string LinkName { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Date { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Class { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Unit { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Type { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string IsLink { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Override { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Cookie { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Comments { get; set; }
+    }
+
+    /// <summary>
+    /// This data model is used to receive information for a file to be deleted.
+    /// </summary>
+    [DataContract]
+    public class DeleteData
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public string Name { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Class { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Cookie { get; set; }
+    }
+
+    /// <summary>
+    /// This data model is used when the server is sending file contents to the client, per the client's request.
+    /// </summary>
+    [DataContract]
+    public class FileContents
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public string File { get; set; }
+    }
+}
