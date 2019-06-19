0be96dcf3a8bd396114db41f9b4a95f57b5aa033
Jacob Floisand <jacobfloisand@gmail.com>
2019-06-18 21:36:31 -0600

    Implement GetFullFile function.

--- FilerService2.0/FilerService2.0.svc.cs
+++ FilerService2.0/FilerService2.0.svc.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace FilerService2._0
@@ -11,6 +15,16 @@ namespace FilerService2._0
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class FilerService2 : IFilerService2
     {
+        private static string FilerDB2;
+        private static SqlConnection FilerDB2Connection;
+
+        static FilerService2()
+        {
+            FilerDB2 = ConfigurationManager.ConnectionStrings["FilerDB2"].ConnectionString;
+            FilerDB2Connection = new SqlConnection(FilerDB2);
+            FilerDB2Connection.Open();
+        }
+
         public void AddFile(ResourceDataVerbose data)
         {
             throw new NotImplementedException();
@@ -28,13 +42,44 @@ namespace FilerService2._0
 
         public FileContents GetFullFile(string Name, string Class, string Cookie)
         {
-            /*
-            SELECT Files.Archive FROM Files JOIN Classes ON Files.DataID = Classes.DataID
-            WHERE Files.Name = '1210 articles_s18.pdf' AND Classes.Class = 'Biology' INTERSECT
-            SELECT UserIDs.DataID FROM UserIDs JOIN Cookies ON UserIDs.UserID = Cookies.UserID
-            WHERE Cookies.Cookie = 'tHiSIsaCoOKie3857957';
-            */
-            throw new NotImplementedException();
+            if (Name == null || Class == null || Cookie == null)
+            {
+                SetStatus(HttpStatusCode.Forbidden);
+                return null;
+            }
+            string GetFileContentsQuery = "SELECT Files.Archive FROM Files JOIN Classes ON Files.DataID = Classes.DataID " +
+                                          "WHERE Files.Name = @Name AND Classes.Class = @Class INTERSECT " +
+                                          "SELECT UserIDs.DataID FROM UserIDs JOIN Cookies ON UserIDs.UserID = Cookies.UserID " +
+                                          "WHERE Cookies.Cookie = @Cookie;";
+
+            using (SqlCommand command = new SqlCommand(GetFileContentsQuery, FilerDB2Connection))
+            {
+                command.Parameters.AddWithValue("@Name", Name);
+                command.Parameters.AddWithValue("@Class", Class);
+                command.Parameters.AddWithValue("@Cookie", Cookie);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    //If there's something to read then it's the file contents.
+                    if (reader.Read())
+                    {
+                        return new FileContents
+                        {
+                            File = reader.GetString(0)
+                        };
+                    }
+                    //If there's nothing to read then no file matched the Name, class, cookie combo.
+                    else
+                    {
+                        SetStatus(HttpStatusCode.Conflict);
+                        return null;
+                    }
+                }
+            }
+        }
+
+        private static void SetStatus(HttpStatusCode status)
+        {
+            WebOperationContext.Current.OutgoingResponse.StatusCode = status;
         }
     }
 }
